30137a386dcaa9ae54b8438589bb7c0b955d04e4
Feyzan Aydemir <feyzan.aydemir@gmail.com>
2021-12-19 22:28:55 +0300

    sad

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,7 +7,7 @@ function About() {
       <h2>Engineer & Developer</h2>
       <p>
         I'm an enthusiastic engineer with a passion for the art of code. I build
-        web applications and primarily work with Node.js, Express.js, MongoDB,
+        web applications & primarily work with Node.js, Express.js, MongoDB,
         React, JavaScript, CSS & HTML.
       </p>
     </div>
